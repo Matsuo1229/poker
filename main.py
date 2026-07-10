6fb7e66cc23308a51875dafa5cc0ab0af5f20681
Matsuo1229 <yuto.mto1229@gmail.com>
2026-07-10 20:38:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -331,8 +331,8 @@ async def main():
 
             txt = font.render("Press Space",True,WHITE)
             screen.blit(txt, [300,500])
-#            if key[pygame.K_SPACE] == 1: #ここらに画像はりたい
-            if event.type == MOUSEBUTTONDOWN:
+            if key[pygame.K_SPACE] == 1: #ここらに画像はりたい
+  #          if event.type == MOUSEBUTTONDOWN:
                  idx = 2
 
 #        elif idx == 1:
